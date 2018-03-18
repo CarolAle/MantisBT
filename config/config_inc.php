ad74c5a7f36c794d68882c23e17db81eb3550875
carolina <carolinalessandra2@gmail.com>
2018-03-18 15:25:25 -0300

    subindo mantis

---
+++ config/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+
+$g_default_timezone       = 'America/Sao_Paulo';
+
+$g_crypto_master_salt     = 'iRQTtJ51RrDy9zlkOKjKw+pSm3YtUrY0qpQ93REEP50=';
+
+$g_default_language = 'portuguese_brazil'; 
